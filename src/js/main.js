2e4d6c7dc74929476abdce0bfb99cd7762f79149
Willie <shibelgut.dev@gmail.com>
2021-03-01 12:39:36 +0600

    use classes

--- src/js/main.js
+++ src/js/main.js
@@ -110,17 +110,22 @@ window.addEventListener('DOMContentLoaded', () => {
         closeModal = document.querySelector('[data-close]'),
         modal = document.querySelector('.modal');
 
+    function open() {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId); // не показывать модальное, если открыл сам
+    }
+
     triggerModal.forEach(i => {
-        i.addEventListener('click', () => {
-            modal.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        });
+        i.addEventListener('click', open);
     });
 
     function close() {
         modal.style.display = 'none';
         document.body.style.overflow = '';
     }
+
+
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
@@ -135,4 +140,68 @@ window.addEventListener('DOMContentLoaded', () => {
             close();
         }
     });
+
+
+    //Отложенная загрузка модального окна
+
+    // const modalTimerId = setTimeout(open, 3000);
+
+    // function showModalByScroll() {
+    //     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    //         open();
+    //         window.removeEventListener('scroll', showModalByScroll);
+    //     }
+    // }
+
+    // //Загрузка после докручивания до конца страницы
+
+    // window.addEventListener('scroll', showModalByScroll);
+
+
+//Используем классы для карточек меню
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+
+    ).render();
+
 });
